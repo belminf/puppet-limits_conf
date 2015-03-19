2237ec420c92cd163955b51311d8c043ba2e7b0a
Belmin Fernandez <me@iambelmin.com>
2015-03-19 09:13:41 -0400

    added example to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,12 @@
 #
 # == Sample Usage:
 #
-# class { 'limits_conf': }
+# limits_conf::set {
+#     'soft-nproc':     item => 'nproc',    value => '16384';
+#     'soft-nofile':    item => 'nofile',   value => '65536';
+#     'hard-nproc':     item => 'nproc',    value => '16384',   type => 'hard';
+#     'hard-nofile':    item => 'nofile',   value => '65536',   type => 'hard';
+# }
 
 class limits_conf {
 
